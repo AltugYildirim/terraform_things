bf3e1492a372dfde7d41013ad4bbe1c95fe49383
Altug Yildirim <32032244+AltugYildirim@users.noreply.github.com>
2019-09-12 23:57:24 +0300

    Update and rename mapping_v5.ps1 to mapping_v6.ps1

--- mapping_v5.ps1
+++
@@ -1,2 +0,0 @@
-cmdkey /add:testforlcw.file.core.windows.net /user:Azure\testforlcw /pass:vEaiyUmP1DEIrushnVT5jZnFdh9uvTrmfBA/AuyC47kARfFqYXgIUsKc6jF7+EF6ESIrjQEbJ1JYNXs2nPUC8g==
-net use Z: \\testforlcw.file.core.windows.net\lcwv1 /persistent:Yes /user:altug 514Passwrd
