3b627a090a124c01b93bc65e630acd0bbc75be36
Altug Yildirim <32032244+AltugYildirim@users.noreply.github.com>
2019-09-12 14:39:07 +0300

    Update dockerpull.ps1

--- dockerpull.ps1
+++ dockerpull.ps1
@@ -1,2 +1,2 @@
-mkdir "C:\deployment"
-docker pull mcr.microsoft.com/dotnet/framework/wcf
+cmdkey /add:sourcecodelcw.file.core.windows.net /user:Azure\sourcecodelcw /pass:w80CrpBrGaOFnE6B78prV2NVLk+NfmgLFKLWoMTcqiCPT0SctVHZjwpsfZqN62ubRQDrTN47hEL9uNSUzwSvqg==
+net use Z: \\sourcecodelcw.file.core.windows.net\lcw /persistent:Yes
