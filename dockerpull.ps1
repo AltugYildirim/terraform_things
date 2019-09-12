0edb058cd2475bf250f8defa1462d94dbad36914
Altug Yildirim <32032244+AltugYildirim@users.noreply.github.com>
2019-09-12 21:55:18 +0300

    Update dockerpull.ps1

--- dockerpull.ps1
+++ dockerpull.ps1
@@ -1,2 +1,2 @@
-Invoke-Expression -Command "cmdkey /add:sourcecodelcw.file.core.windows.net /user:Azure\sourcecodelcw /pass:w80CrpBrGaOFnE6B78prV2NVLk+NfmgLFKLWoMTcqiCPT0SctVHZjwpsfZqN62ubRQDrTN47hEL9uNSUzwSvqg=="
-New-PSDrive -Name Z -PSProvider FileSystem -Root "\\sourcecodelcw.file.core.windows.net\lcw"
+cmdkey /add:sourcecodelcw.file.core.windows.net /user:Azure\sourcecodelcw /pass:w80CrpBrGaOFnE6B78prV2NVLk+NfmgLFKLWoMTcqiCPT0SctVHZjwpsfZqN62ubRQDrTN47hEL9uNSUzwSvqg==
+net use Z: \\sourcecodelcw.file.core.windows.net\lcw /persistent:Yes
