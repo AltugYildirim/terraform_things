3cb81301a8ba012df2cbaa0475f448ee600e5f40
Altug Yildirim <32032244+AltugYildirim@users.noreply.github.com>
2019-09-12 21:54:28 +0300

    Update dockerpull.ps1

--- dockerpull.ps1
+++ dockerpull.ps1
@@ -1,4 +1,2 @@
-# Save the password so the drive will persist on reboot
 Invoke-Expression -Command "cmdkey /add:sourcecodelcw.file.core.windows.net /user:Azure\sourcecodelcw /pass:w80CrpBrGaOFnE6B78prV2NVLk+NfmgLFKLWoMTcqiCPT0SctVHZjwpsfZqN62ubRQDrTN47hEL9uNSUzwSvqg=="
-# Mount the drive
 New-PSDrive -Name Z -PSProvider FileSystem -Root "\\sourcecodelcw.file.core.windows.net\lcw"
