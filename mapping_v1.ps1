364ad549c8074fac57a9d999472a10542a0aba33
Altug Yildirim <32032244+AltugYildirim@users.noreply.github.com>
2019-09-12 22:00:23 +0300

    Update and rename mapping.ps1 to mapping_v1.ps1

--- mapping_v1.ps1
+++ mapping_v1.ps1
@@ -1,2 +1,3 @@
 cmdkey /add:sourcecodelcw.file.core.windows.net /user:Azure\sourcecodelcw /pass:w80CrpBrGaOFnE6B78prV2NVLk+NfmgLFKLWoMTcqiCPT0SctVHZjwpsfZqN62ubRQDrTN47hEL9uNSUzwSvqg==
+sleep 10
 net use Z: \\sourcecodelcw.file.core.windows.net\lcw /persistent:Yes
