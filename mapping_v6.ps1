0394c2bdfe945f566f017c0cd63d4549d512606d
Altug Yildirim <32032244+AltugYildirim@users.noreply.github.com>
2019-09-12 23:57:47 +0300

    Update mapping_v6.ps1

--- mapping_v6.ps1
+++ mapping_v6.ps1
@@ -1,2 +1,2 @@
-runas /user:serviceaccountname "%windir%\system32\cmdkey /add:testforlcw.file.core.windows.net /user:Azure\testforlcw /pass:vEaiyUmP1DEIrushnVT5jZnFdh9uvTrmfBA/AuyC47kARfFqYXgIUsKc6jF7+EF6ESIrjQEbJ1JYNXs2nPUC8g=="
+runas /user:altug "%windir%\system32\cmdkey /add:testforlcw.file.core.windows.net /user:Azure\testforlcw /pass:vEaiyUmP1DEIrushnVT5jZnFdh9uvTrmfBA/AuyC47kARfFqYXgIUsKc6jF7+EF6ESIrjQEbJ1JYNXs2nPUC8g=="
 net use Z: \\testforlcw.file.core.windows.net\lcwv1 /persistent:Yes /user:altug 514Passwrd
