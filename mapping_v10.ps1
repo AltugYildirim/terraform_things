f6d98f6fe3c8ce7d22bcf3a29f1c4b5df66a0e6b
Altug Yildirim <32032244+AltugYildirim@users.noreply.github.com>
2019-09-13 13:40:49 +0300

    Update mapping_v10.ps1

--- mapping_v10.ps1
+++ mapping_v10.ps1
@@ -1,5 +1,6 @@
-cmdkey /add:sourcecodelcw.file.core.windows.net /user:Azure\sourcecodelcw /pass:Oa+K24afkqbBn9Go5k1I84T+cM9/MMJKC7+MUq/+/g/JAvFwScmF9GxxKQEOj3DE+Y8bqfH7pwxnqTsFwHdOmQ==
-net use Z: \\sourcecodelcw.file.core.windows.net\lcw /persistent:Yes
+cmdkey /add:testforlcw.file.core.windows.net /user:Azure\testforlcw /pass:vEaiyUmP1DEIrushnVT5jZnFdh9uvTrmfBA/AuyC47kARfFqYXgIUsKc6jF7+EF6ESIrjQEbJ1JYNXs2nPUC8g==
+net use Z: \\testforlcw.file.core.windows.net\lcwv1 /persistent:Yes
 sleep 10
 whoami >> "C:\deployment\var.txt"
-whoami >> "\\sourcecodelcw.file.core.windows.net\lcw\test1.txt"
+whoami >> "\\testforlcw.file.core.windows.net\lcwv1\test1long.txt"
+whoami >> "Z:\test1z.txt"
