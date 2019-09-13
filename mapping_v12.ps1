5cd9e8fb1502615429c7e0652465c2ff38a9f3ce
Altug Yildirim <32032244+AltugYildirim@users.noreply.github.com>
2019-09-13 15:26:57 +0300

    Update and rename mapping_v12.ps1 to mapping_v13.ps1

--- mapping_v12.ps1
+++
@@ -1,5 +0,0 @@
-cmdkey /add:testforlcw.file.core.windows.net /user:Azure\testforlcw /pass:vEaiyUmP1DEIrushnVT5jZnFdh9uvTrmfBA/AuyC47kARfFqYXgIUsKc6jF7+EF6ESIrjQEbJ1JYNXs2nPUC8g==
-net use Z: \\testforlcw.file.core.windows.net\lcwv1 /persistent:Yes
-sleep 10
-whoami >> "C:\deployment\var.txt"
-whoami >> "\\testforlcw.file.core.windows.net\lcwv1\test1long1.txt"
---
+++ mapping_v13.ps1
@@ -0,0 +1,2 @@
+cmdkey /add:ecommercestorageaccount.file.core.windows.net /user:Azure\ecommercestorageaccount /pass:vyzYorDdPhAoMt3fH3e00qeAVkDVWoEOfA4Rw+5YSO2r7xJCMJmY3rOEtv9l14nE3de33ndkYhCtjOLwgxo+cw==
+net use Z: \\ecommercestorageaccount.file.core.windows.net\ecommercefiles /persistent:Yes
