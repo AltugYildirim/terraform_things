34514f935846a74ca7942df4083a92d142ea6d2c
Altug Yildirim <32032244+AltugYildirim@users.noreply.github.com>
2019-09-13 13:37:57 +0300

    Update and rename mapping_v9.ps1 to mapping_v10.ps1

--- mapping_v10.ps1
+++ mapping_v10.ps1
@@ -1,3 +1,4 @@
 cmdkey /add:sourcecodelcw.file.core.windows.net /user:Azure\sourcecodelcw /pass:Oa+K24afkqbBn9Go5k1I84T+cM9/MMJKC7+MUq/+/g/JAvFwScmF9GxxKQEOj3DE+Y8bqfH7pwxnqTsFwHdOmQ==
 net use Z: \\sourcecodelcw.file.core.windows.net\lcw /persistent:Yes
-whoami >> "Z:\test1.txt"
+whoami >> "C:\deployment\var.txt"
+whoami >> "\\sourcecodelcw.file.core.windows.net\lcw\test1.txt"
